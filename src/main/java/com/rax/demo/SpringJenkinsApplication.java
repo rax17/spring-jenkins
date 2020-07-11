1bc37f9068b6dfb1630e035aac87ca607fa48235
Rakesh kumar <rax17@icloud.com>
2020-07-11 06:10:04 -0400

    first commit

---
+++ src/main/java/com/rax/demo/SpringJenkinsApplication.java
@@ -0,0 +1,26 @@
+package com.rax.demo;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	
+	public static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplication.class.getName());
+	
+	@PostConstruct
+	public void init() {
+		logger.info(" init () Application started ...");
+		
+	}
+
+	public static void main(String[] args) {
+		logger.info("Application executed ...");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
